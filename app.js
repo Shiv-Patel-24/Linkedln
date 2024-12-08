2b87f932b41f0f0b05a6bc167aa4d0f9c4b2e5dd
Shiv <pshiv2411@gmail.com>
2024-12-08 22:58:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// let asking = prompt("Enter your full name here");
+// console.log(asking);
+// let trimingTheAsking = asking.trim();
+// let findingLength = trimingTheAsking.length;
+// let mergeTwo = `@${asking}${trimingTheAsking.length}`;
+// console.log(mergeTwo);
+// let mergeTwo2 = "@"+asking.concat(trimingTheAsking.length);
+// console.log(mergeTwo2);
+
+
+let accessCont = document.getElementById("login_with_google")
+accessCont.style.fontWeight = "700";
+
+let accessUsingId  = document.getElementById("dec_font-size");
+let changeSomething = accessUsingId.style.fontSize = "13.5px"
+
+
+
+btn.style.fontSize = "300px"
+
+// console.log("@",trimingTheAsking);
+// console.log("person name length is",findingLength);
+// console.log(`person name length is ${findingLength}`);
